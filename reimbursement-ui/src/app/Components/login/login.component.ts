42440f1b95ea619c0263983747abd645eb29df99
Jacob Dean <JacobDean117@gmail.com>
2019-04-25 15:07:40 -0400

    created angular project and add html login page

---
+++ reimbursement-ui/src/app/Components/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import {Router, ActivatedRoute} from '@angular/router';
+import {FormBuilder, FormGroup, Validator, Validators} from '@angular/forms';
+import {first} from 'rxjs/operators';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    
+  ) { }
+    authUser: any;
+    debug = true;
+
+  ngOnInit() {
+  }
+
+}
